1207614e13f53bc30526735a565a8f5f05899866
Z4ck <128758112+Z4ckStudios@users.noreply.github.com>
2024-07-20 09:50:09 -0600

    Add files via upload

---
+++ Javascript!404.js
@@ -0,0 +1,32 @@
+var displays = ["inline", "none"];
+var i = 2;
+
+var url = "https://Z4ckStudios.github.io/SnowwNet/404/";
+window.history.replaceState(null, document.title, url);
+
+document.querySelector(".DevEmailButton").addEventListener("click",
+function(){
+    i = i < displays.length ? ++i : i - 2;
+    i = i < displays.length ? +i : i - 2;
+
+    document.querySelector(".DevEmailButton").style.display = "none";
+    document.querySelector(".DevEmailButtonCallback").style.display = "inline";
+
+    document.querySelector(".DevEmailText").style.display = displays[i];
+    document.querySelector(".DevEmailTextRefade").style.display = "none";
+})
+
+var displays = ["inline", "none"];
+var i = 2;
+
+document.querySelector(".DevEmailButtonCallback").addEventListener("click",
+function(){
+    i = i < displays.length ? ++i : i - 2;
+    i = i < displays.length ? +i : i - 2;
+
+    document.querySelector(".DevEmailButton").style.display = "inline";
+    document.querySelector(".DevEmailButtonCallback").style.display = "none";
+
+    document.querySelector(".DevEmailText").style.display = "none";
+    document.querySelector(".DevEmailTextRefade").style.display = "inline";
+})
