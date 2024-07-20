06f71af0cc6b20a039258b1f44d1e0c50d69ea40
Z4ck <128758112+Z4ckStudios@users.noreply.github.com>
2024-07-20 08:47:57 -0600

    Add files via upload

--- Javascript!index.js
+++ Javascript!index.js
@@ -9,15 +9,17 @@ window.history.replaceState(null, document.title, url);
 function StartTimerInt() {
     StartTimer++
 
-    if(StartTimer > 1 && SiteLocation == null || StartTimer > 1 && SiteLocation == "MainPage")
+    if(StartTimer > 1)
+        {
+            if(SiteLocation == null || SiteLocation == "MainPage")
         {location.replace("https://Z4ckStudios.github.io/SnowwNet/MainPage.html");}
-    else
-    if(StartTimer > 1 && SiteLocation == "AboutUs")
+            else
+        if(SiteLocation == "AboutUs")
         {location.replace("https://Z4ckStudios.github.io/SnowwNet/AboutUs.html");}
-    else
-    if(StartTimer > 1 && SiteLocation == "ServerStatus")
+        else
+        if(SiteLocation == "ServerStatus")
         {location.replace("https://Z4ckStudios.github.io/SnowwNet/ServerStatus.html");}
-    else
-    if(StartTimer > 1 && SiteLocation == "Home")
-        {location.replace("https://Z4ckStudios.github.io/SnowwNet/Home.html");}
+        else
+        if(SiteLocation == "Home")
+        {location.replace("https://Z4ckStudios.github.io/SnowwNet/Home.html");}}
 }
